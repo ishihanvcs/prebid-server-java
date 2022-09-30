7a21195504e0b62580e0d3c10e48f59f8826c962
Afsar Uddin (Old) <afsar.uddin@widespace.com>
2022-09-30 04:36:49 +0000

    Merged in HBT-265-auddin-more-it-tests (pull request #78)
    HBT-265 more it tests. Heavy refactoring in it test cases.
    
    * Test case added for SupplyChain addition when any imp has schain nodes
    
    * testCustomVastResponseWithMultiFormatToMultipleBidders() is now multi-imp + multi-format + multi-bidder test case
    
    * Test case added for: responseType=waterfall and test=1
    
    * Test case added for: /gvast?debug=1
    
    * Supply chain pbs config searching is tweaked a bit.
    
    * Supply chain pbs config searching is tweaked again a bit.
    
    * Supply chain's multi-imp settings is refactored
    
    * Non-USD currency (in auction) test case added
    
    * Bid precesion testing is fixed
    
    * Supply chain related changes are removed
    
    
    Approved-by: Jozef Bartek

---
+++ src/test/java/com/improvedigital/prebid/server/it/transformers/BidResponseFunctionByImpidTransformer.java
@@ -0,0 +1,45 @@
+package com.improvedigital.prebid.server.it.transformers;
+
+import com.github.tomakehurst.wiremock.common.FileSource;
+import com.github.tomakehurst.wiremock.extension.Parameters;
+import com.github.tomakehurst.wiremock.http.Response;
+import com.iab.openrtb.request.BidRequest;
+
+import java.util.function.Function;
+
+public class BidResponseFunctionByImpidTransformer extends ImprovedigitalResponseTransformer {
+
+    @Override
+    public String getName() {
+        return "it-test-bid-response-function-by-impid";
+    }
+
+    @Override
+    public Response transform(
+            com.github.tomakehurst.wiremock.http.Request request,
+            Response response,
+            FileSource fileSource,
+            Parameters parameters) {
+        try {
+            BidRequest bidRequest = parseRequest(request, BidRequest.class);
+            if (bidRequest.getImp().size() != 1) {
+                throw new IllegalArgumentException("SSP can deal only 1 imp");
+            }
+
+            String impId = bidRequest.getImp().get(0).getId();
+            Function<BidRequest, String> f = (Function<BidRequest, String>) parameters.get(impId);
+            if (f == null) {
+                throw new IllegalArgumentException("No function found to deal with imp id=" + impId);
+            }
+
+            String bidResponse = f.apply(bidRequest);
+            if (bidResponse == null) {
+                throw new IllegalArgumentException("No bid response found for imp id=" + impId);
+            }
+
+            return getSuccessResponse(bidResponse);
+        } catch (Exception e) {
+            return getErrorResponse(e);
+        }
+    }
+}
